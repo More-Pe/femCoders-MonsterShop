251907a078c0ea642f05221df0d5e325733e6ba1
More-Pe <peraltamorena.va@gmail.com>
2025-06-16 20:27:43 +0200

    add Product an Review models

--- src/main/java/com/example/monsterShop/models/Review.java
+++ src/main/java/com/example/monsterShop/models/Review.java
@@ -1,4 +1,26 @@
 package com.example.monsterShop.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "reviews")
 public class Review {
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;
+
+   @Column(nullable = false, unique = true)
+   private String username;
+
+   @Column(nullable = false)
+   double rating;
+
+   @Column(nullable = false)
+   String body;
+
+   @ManyToOne(fetch = FetchType.LAZY)
+   @JoinColumn(name = "product_id")
+   private Product product;
+
+
 }
